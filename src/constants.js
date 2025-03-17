f5ec73f32a4c2cc7ffea76f42e5bb16cbbf2d23c
Kartik Jha <kartik.n.jha@gmail.com>
2025-03-17 17:25:08 +0530

    fix: constants file updated

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const API_BASE = 'https://engaging-weevil-preferably.ngrok-free.app';
-export const WS_BASE = 'ws://localhost:8000/ws';
+export const WS_BASE = 'wss://engaging-weevil-preferably.ngrok-free.app/ws';
+// export const API_BASE = 'https://battleshipbackend-production.up.railway.app';
+// export const WS_BASE = 'wss://battleshipbackend-production.up.railway.app/ws';
